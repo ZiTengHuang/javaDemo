38804c5a4f33b27a96f0e32d7476cbe1e0c3713b
ZiTengHuang <hzt.nba2007@qq.com>
2017-09-16 19:01:07 +0800

    创建程序。#32

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    /**
+     *  此处将会实现大量的java例子 每天做一点更新一点。
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
